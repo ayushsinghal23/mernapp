494b1efdef4422810c5116eec023d8736371b5ec
Ayush Singhal <ayush.singhal_cs18@gla.ac.in>
2021-04-14 02:01:33 +0530

    set up the redux store for cart and products

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,12 +1,27 @@
 import './CartScreen.css';
-
+import CartItem from '../components/Cartitem';
 const CartScreen = () => {
     return (
         <div className="cartscreen">
            <div class="cartscreen__left">
                <h2>Shopping Cart</h2>
+
+               <CartItem/>
+               <CartItem/>
+               <CartItem/>
+               <CartItem/>
+
            </div>
-           <div class="cartscreen__right"></div>
+           <div class="cartscreen__right">
+               <div className="cartScreen__info">
+                   <p>Subtotal (0) items</p>
+                   <p>$499.99</p>
+               </div>
+               <div>
+                   <button>Proceed to Checkout
+                   </button>
+               </div>
+           </div>
         </div>
     )
 }
